8e82f6d1148c46946b2848f8a1701857f41e76fd
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-10-24 23:19:40 +0600

    responsive update & code optimized

--- src/category/Category.jsx
+++ src/category/Category.jsx
@@ -4,17 +4,17 @@ import NotAvail from './NotAvail';
 import Cards from './Cards';
 
 const Category = () => {
-  const { brand } = useParams()
+  const { brand } = useParams();
   const cars = useLoaderData();
   const brandCar = cars.filter((car)=>car.brand.toLowerCase()==brand.toLowerCase());
   return (
+    brandCar.length?
     <div className='container m-auto grid md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-10 px-10 py-20'>
       {
-        brandCar.length?
-        brandCar.map((car)=><Cards key={car._id} car={car}></Cards>) :
-        <NotAvail></NotAvail>
+        brandCar.map((car)=><Cards key={car._id} car={car}></Cards>)
       }
-    </div>
+    </div>: 
+    <NotAvail></NotAvail>
   );
 };
 
--- src/home/Cars.jsx
+++ src/home/Cars.jsx
@@ -2,17 +2,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 const Cars = ({car}) => {
   return (
-    <Link to={`/category/${car.name}`}>
-      <div className="container m-auto card w-full h-full bg-base-100 shadow-xl image-full">
-        <figure>
-          <img
-            src={car.img_link}
-            alt={car.name}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title m-auto text-3xl md:text-5xl">{car.name}</h2>
-        </div>
+    <Link to={`/category/${car.name}`} className="container m-auto card w-full h-full bg-base-100 shadow-xl image-full">
+      <figure className='w-full'>
+        <img
+          src={car.img_link}
+          alt={car.name}
+          className='w-full h-full object-cover'
+        />
+      </figure>
+      <div className="card-body w-full h-full ">
+        
+        <h2 className="card-title m-auto text-3xl md:text-3xl">
+          <img src={car.logo} alt="logo" className='h-10 w-10 content-center object-cover rounded-full' />
+          {car.name}
+        </h2>
       </div>
     </Link>
   );
